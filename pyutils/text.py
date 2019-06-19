61a721a3a756f5bf0db5adf8968d219ae5a8a3c4
jtr109 <lyp_login@outlook.com>
2019-06-19 13:40:34 +0800

    feat(pu): add comment

--- pyutils/text.py
+++ pyutils/text.py
@@ -5,9 +5,18 @@ _underscorer2 = re.compile('([a-z0-9])([A-Z])')
 
 
 def camel2snake(camel: str) -> str:
+    ''' Convert From Camel Case To Snake Case
+
+    reference: https://gist.github.com/jaytaylor/3660565
+    '''
     subbed = _underscorer1.sub(r'\1_\2', camel)
     return _underscorer2.sub(r'\1_\2', subbed).lower()
 
 
 def snake_to_camel(snake: str) -> str:
+    ''' Convert From Snake Case To Camel Case
+
+    reference: https://rodic.fr/blog/camelcase-and-snake_case-strings-conversion-with-python/
+    '''
+
     return re.sub(r'(?:^|_)([a-z])', lambda x: x.group(1).upper(), snake)

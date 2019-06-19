042f797f9cf8f0c1a26cfcd9fcfbc0e345f23625
jtr109 <lyp_login@outlook.com>
2019-06-19 22:49:57 +0800

    feat(pu): use setuptools in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
-setup(name='pyutils',
-    version='0.02',
+
+setup(
+    name='pyutils',
+    version='0.03',
     description='Utils for coding Python.',
     author='jtr109',
     author_email='conbas2019@gmail.com',
